51f448aa4eec00d38683c0b899207a6b406eb9d7
Zhenning Yu <yuzhenning.bio@gmail.com>
2017-03-21 08:57:34 -0400

    Add SPRT test

--- SPRT/SPRT.py
+++ SPRT/SPRT.py
@@ -13,22 +13,33 @@ class SPRT:
     def __init__(self, alpha = 0.05, beta = 0.2,
                  h0 = 0, h1 = 1, values = []):
 
+        # Input arguments
         self.alpha = alpha
         self.beta = beta
         self.h0 = h0
         self.h1 = h1
         self.values = values
+        # Necessary arguments
         self.upperCritical = np.log((1 - self.beta)/self.alpha)
         self.lowerCritical = np.log((1 - self.alpha)/self.beta)
         self.num_observation = len(values)
         self.seq_observation = np.array(range(1, self.num_observation + 1))
         # Check the arguments
-        self.checkArgs()
+        self.checkCommonArgs()
+        self.checkOtherArgs()
         # Calculate boundary
         self.calBoundary()
         # Sequential test
         self.seqTest()
-        
+
+    # Check common arguments in the fuction
+    def checkCommonArgs(self):
+
+        if not all(0 < i <1 for i in [self.alpha, self.beta]):
+
+            sys.stderr.write("Type I error rate and type II error rate are between 0 and 1!")
+            sys.exit(1)
+
     # Plot the boundary and points
     def plot(self, boundaryColor = ["#b14a55", "#fe6b7a"]):
 
@@ -39,7 +50,7 @@ class SPRT:
         plt.show()
 
     # Plot only the boundary values
-    def plot_boundary(self):
+    def plotBoundary(self):
 
         fig, ax = plt.subplots(figsize=(8, 4))
         ax.plot(self.seq_observation, self.upperBoundary, color="#b14a55")
@@ -50,10 +61,11 @@ class SPRT:
     @abc.abstractmethod
     def calBoundary(self):
 
-        return 
+        return
 
-    # Abstarct method, function to check the input argument
-    def checkArgs(self):
+    # Abstarct method, function to check other input arguments
+    @abc.abstractmethod
+    def checkOtherArgs(self):
 
         return 
             
@@ -67,6 +79,7 @@ class SPRT:
 class SPRTBinomial(SPRT):
 
     """Run sequential probability ratio test (SPRT) for bindary endpoints"""
+    # Calculate boundary for binary outcome
     def calBoundary(self):
 
         self.denom = (np.log(self.h1/(1 - self.h1)) - np.log(self.h0/(1 - self.h0)))
@@ -75,10 +88,36 @@ class SPRTBinomial(SPRT):
         self.upperBoundary = self.seq_observation * self.slope + self.upperIntercept
         self.lowerBoundary = self.seq_observation * self.slope + self.lowerIntercept
 
+    # Check arguments
+    def checkOtherArgs(self):
+
+        # Check h0 and h1
+        if not all(0 < i <1 for i in [self.h0, self.h1]):
+
+            sys.stderr.write("Null and alternative values are between 0 and 1!")
+            sys.exit(1)
+
+        # Check values
+        if not all(i in [0, 1] for i in self.values):
+
+            sys.stderr.write("Value is a Beroulli variable!")
+            sys.exit(1)
+
+    # Sequential test
     def seqTest(self):
 
         self.cum_values = np.cumsum(self.values)
+        self.test_statistic = self.cum_values[self.num_observation - 1]
+        if self.test_statistic > self.upperBoundary[self.num_observation - 1]:
 
+            self.decision = "Reject"
 
-a = SPRTBinomial(alpha = 0.05, beta = 0.2, h0 = 0.1, h1 = 0.3, values = [1, 1, 0, 1, 0, 1])
-a.plot_boundary()  
+        elif self.test_statistic < self.lowerBoundary[self.num_observation - 1]:
+
+            self.decision = "Accept"
+
+        else:
+
+            self.decision = "Continue"
+
+        return self.decision
